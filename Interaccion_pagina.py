fbd9ebbd5c6b4a19c623de17cb2b21ab720bf696
Javierov2201 <33948592+Javierov2201@users.noreply.github.com>
2017-11-24 09:19:07 -0500

    Add files via upload

--- Interaccion_pagina.py
+++ Interaccion_pagina.py
@@ -1,38 +1,14 @@
-import urllib.request
 import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 from PIL import Image
-from datetime import datetime
 import sqlite3
 import cv2
 
 ##DESCARGAR EL CHROMEDRIVER DE https://chromedriver.storage.googleapis.com/index.html
 ##EXTRAER EL chromedriver.exe EN EL DIRECTORIO DONDE ESTA EL PYTHON (p.e. C:\python36-32)
 ##SI ESTA TODO BIEN, EN CMD EL COMANDO chromedriver -v DEBERIA DAR LA VERSION, SINO AGREGAR LA RUTA AL PATH
-
-#### Never do this -- insecure!
-##symbol = 'RHAT'
-##c.execute("SELECT * FROM stocks WHERE symbol = '%s'" % symbol)
-##
-#### Do this instead
-##t = ('RHAT',)
-##c.execute('SELECT * FROM stocks WHERE symbol=?', t)
-##print(c.fetchone())
-##
-#### Larger example that inserts many records at a time
-##purchases = [('2006-03-28', 'BUY', 'IBM', 1000, 45.00),
-##             ('2006-04-05', 'BUY', 'MSFT', 1000, 72.00),
-##             ('2006-04-06', 'SELL', 'IBM', 500, 53.00),
-##            ]
-##c.executemany('INSERT INTO stocks VALUES (?,?,?,?,?)', purchases)
-
-##CREATE TABLE artist();
-##CREATE TABLE track(
-##FOREIGN KEY (trackartist) REFERENCES artist(artistid));
-
-##PRIMARY KEY (column1, column2)
 
 def crear_bd() :
     conn = sqlite3.connect("MEF.db")
@@ -87,9 +63,9 @@ def crear_bd() :
 def insertar_datos(uuee, expediente, datos_expediente) :
     conn = sqlite3.connect("MEF.db")
     cur = conn.cursor()
-    cur.executemany('INSERT INTO UUEE VALUES (?,?)', uuee)
-    cur.executemany('INSERT INTO Expediente VALUES (?,?,?,?,?,?,?,?,?)', expediente)
-    cur.executemany('INSERT INTO DatosExpediente VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)', datos_expediente)
+    cur.executemany('INSERT OR IGNORE INTO UUEE VALUES (?,?)', uuee)
+    cur.executemany('INSERT OR IGNORE INTO Expediente VALUES (?,?,?,?,?,?,?,?,?)', expediente)
+    cur.executemany('INSERT OR IGNORE INTO DatosExpediente VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)', datos_expediente)
     conn.commit()
     cur.close()
     conn.close()
@@ -108,7 +84,6 @@ def bajar_registros(driver) :
     expediente += (consulta[7].get_attribute('value'),)  ##Descripcion de la modalidad
     expediente += (consulta[8].get_attribute('value'),)  ##Tipo de seleccion
     expediente += (consulta[9].get_attribute('value'),)  ##Descripcion de seleccion
-    ##Esperar, por si acaso
     time.sleep(1)
     consulta = driver.find_elements_by_tag_name('td')
     datos_expediente = []
@@ -124,24 +99,22 @@ def bajar_registros(driver) :
         datos_expediente[num_fila] += (int(consulta[num_fila*14+3].text),)  ##Num Correlativo
         datos_expediente[num_fila] += (str(consulta[num_fila*14+4].text),)  ##Cod Documento
         datos_expediente[num_fila] += (str(consulta[num_fila*14+5].text),)  ##Num Documento
-        ##datos_expediente[num_fila].append(datetime.strptime(consulta[num_fila*14+6].text, "%d/%m/%y"))  ##Fecha
         datos_expediente[num_fila] += (str(consulta[num_fila*14+6].text).replace("/", "-"),)  ##Fecha
         datos_expediente[num_fila] += (str(consulta[num_fila*14+7].text),)  ##FF
         datos_expediente[num_fila] += (str(consulta[num_fila*14+8].text),)  ##Moneda
         datos_expediente[num_fila] += (float(str(consulta[num_fila*14+9].text).replace(",", "")),)  ##Monto
         datos_expediente[num_fila] += (str(consulta[num_fila*14+10].text),)  ##Estado
-        ##datos_expediente[num_fila].append(datetime.strptime(consulta[num_fila*14+11].text, "%d/%m/%y %H:%M:%S"))  ##Fecha hora proceso
         datos_expediente[num_fila] += (str(consulta[num_fila*14+11].text).replace("/", "-"),)  ##Fecha hora proceso
         datos_expediente[num_fila] += (str(consulta[num_fila*14+12].text),)  ##ID_TRX
-
+    
     expediente = [expediente]
     return uuee, expediente, datos_expediente
 
 
 
 def ingresar_datos(driver, captcha_ans, cod_exp, ano_ejec, cod_uuee) :
-    #ano_eje = Select(driver.find_element_by_name('anoEje'))
-    #ano_eje.select_by_value(2017)
+    ano_eje = Select(driver.find_element_by_name('anoEje'))
+    ano_eje.select_by_value('2017')
     sec_eje = driver.find_element_by_name('secEjec')
     sec_eje.send_keys(cod_uuee)
     expediente = driver.find_element_by_name('expediente')
@@ -151,19 +124,16 @@ def ingresar_datos(driver, captcha_ans, cod_exp, ano_ejec, cod_uuee) :
     captcha_form.submit()
 
 
-def resolver_captcha(driver) :##Falta mejorar
+def resolver_captcha(driver) :
     driver.save_screenshot("screenshot.png")
     img=Image.open('screenshot.png')
-    ##Como solo hay un elemento con tag 'img' (el captcha), es sencillo
     captcha_img =img.crop((315,518,515,578))
     captcha_img.save('captcha_img.png')
-    #captcha_img.show()
     img = cv2.imread('captcha_img.png')
     cv2.imshow('Imagen', img)  
     cv2.waitKey(5000)
     cv2.destroyAllWindows()
     captcha_ans = input("Escriba el contenido del captcha: ")
-
     return captcha_ans
 
 
